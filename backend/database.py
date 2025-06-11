d9b4ae4f56d88973bcb3aa358047d34b33956c99
nknusem115 <roberta2026115@gmail.com>
2025-06-11 10:01:56 +0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -10,8 +10,6 @@ load_dotenv()
 # 從環境變數讀取資料庫 URL，如果沒有就用本地 SQLite 作為備用
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./fakenews_detector.db")
 
-
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
